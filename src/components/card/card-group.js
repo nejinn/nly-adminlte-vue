956343ce07bab0eecbba2eed0ca092bbbbb15e56
nejinn <871966163@qq.com>
2020-04-22 23:39:26 +0800

    重构card-group为函数式组件
    Signed-off-by: nejinn <871966163@qq.com>

--- src/components/card/card-group.js
+++ src/components/card/card-group.js
@@ -1,40 +1,43 @@
 import Vue from "../../utils/vue";
 import { cardGroupTypeOption } from "../../utils/nly-config";
 import { nlyGetOptionsByKeyEqual } from "../../utils/get-options";
+import { mergeData } from "vue-functional-data-merge";
+
+export const props = {
+  groupType: {
+    type: String,
+    default: "default"
+  },
+  groupClass: {
+    type: String
+  },
+  tag: {
+    type: String,
+    default: "div"
+  }
+};
+
+const customClass = props => {
+  const groupType = () =>
+    nlyGetOptionsByKeyEqual(cardGroupTypeOption, props.groupType);
+  const groupClass = props.groupClass;
+
+  return [groupType(), groupClass];
+};
 
 const name = "NlyCardGroup";
 
 export const NlyCardGroup = Vue.extend({
   name: name,
-  props: {
-    groupType: {
-      type: String,
-      default: "default"
-    },
-    groupClass: {
-      type: String
-    },
-    tag: {
-      type: String,
-      default: "div"
-    }
-  },
-  computed: {
-    customProps() {
-      return {
-        groupType: nlyGetOptionsByKeyEqual(cardGroupTypeOption, this.groupType),
-        groupClass: this.groupClass,
-        tag: this.tag
-      };
-    }
-  },
-  render(h) {
+  props,
+  functional: true,
+  render(h, { props, data, children }) {
     return h(
-      this.customProps.tag,
-      {
-        class: [this.customProps.groupType, this.customProps.groupClass]
-      },
-      this.$slots.default
+      props.tag,
+      mergeData(data, {
+        class: customClass(props)
+      }),
+      children
     );
   }
 });
