0d5ca5ae074f64042394549e5924eeca77cf22de
nejinn <871966163@qq.com>
2020-04-22 23:33:04 +0800

    重构card-header和card-img为函数式组件
    Signed-off-by: nejinn <871966163@qq.com>

--- src/components/card/card-img.js
+++ src/components/card/card-img.js
@@ -1,26 +1,39 @@
 import Vue from "../../utils/vue";
+import { mergeData } from "vue-functional-data-merge";
+
+export const props = {
+  top: {
+    type: Boolean,
+    default: false
+  },
+  buttom: {
+    type: Boolean,
+    default: false
+  },
+  src: {
+    type: String,
+    required: true
+  },
+  cardImgClass: {
+    type: String
+  }
+};
+
+const customClass = props => {
+  const top = props.top ? "card-img-top" : "";
+  const buttom = props.buttom ? "card-img-buttom" : "";
+  const baseClass = !props.top && !props.buttom ? "card-img" : "";
+  const cardImgClass = props.cardImgClass;
+
+  return [top, buttom, baseClass, cardImgClass];
+};
 
 const name = "NlyCardImg";
 
 export const NlyCardImg = Vue.extend({
   name: name,
-  props: {
-    top: {
-      type: Boolean,
-      default: false
-    },
-    buttom: {
-      type: Boolean,
-      default: false
-    },
-    src: {
-      type: String,
-      required: true
-    },
-    cardImgClass: {
-      type: String
-    }
-  },
+  props,
+  functional: true,
   computed: {
     customProps: function() {
       return {
@@ -34,12 +47,14 @@ export const NlyCardImg = Vue.extend({
       };
     }
   },
-  render(h) {
-    return h("img", {
-      class: [this.customProps.customClass, this.customProps.cardImgClass],
-      attrs: {
-        src: this.customProps.src
-      }
-    });
+  render(h, { props, data, children }) {
+    return h(
+      "img",
+      mergeData(data, {
+        class: customClass(props),
+        src: props.src
+      }),
+      children
+    );
   }
 });
