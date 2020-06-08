9741fb09383ffeec59385557d380a264549e4837
nejinn <871966163@qq.com>
2020-06-08 19:49:31 +0800

    重构栅格布局
    Signed-off-by: nejinn <871966163@qq.com>

--- example/nly-adminlte-vue/components/grid/row.js
+++ example/nly-adminlte-vue/components/grid/row.js
@@ -1,79 +1,69 @@
 import Vue from "../../utils/vue";
+import { mergeData } from "vue-functional-data-merge";
+
+export const props = {
+  tag: {
+    type: String,
+    default: "div"
+  },
+  noGutters: {
+    type: Boolean,
+    default: false
+  },
+  colsXs: {
+    type: String
+  },
+  colsSm: {
+    type: String
+  },
+  colsMd: {
+    type: String
+  },
+  colsLg: {
+    type: String
+  },
+  colsXl: {
+    type: String
+  },
+  rowClass: {
+    String
+  }
+};
+
+const customClass = props => {
+  const customNoGutters = props.noGutters ? "no-gutters" : "";
+  const customColsXs = props.colsXs ? `row-cols-${props.colsXs}` : "";
+  const customColsSm = props.colsSm ? `row-cols-sm-${props.colsSm}` : "";
+  const customColsMd = props.colsMd ? `row-cols-md-${props.colsMd}` : "";
+  const customColsLg = props.colsLg ? `row-cols-lg-${props.colsLg}` : "";
+  const customColsXl = props.colsXl ? `row-cols-lx-${props.colsXl}` : "";
+  const customRowClass = props.rowClass;
+
+  return [
+    customNoGutters,
+    customColsXs,
+    customColsSm,
+    customColsMd,
+    customColsLg,
+    customColsXl,
+    customRowClass
+  ];
+};
 
 const name = "NlyRow";
 
 export const NlyRow = Vue.extend({
   name: name,
-  props: {
-    tag: {
-      type: String,
-      default: "div"
-    },
-    noGutters: {
-      type: Boolean,
-      default: false
-    },
-    colsXs: {
-      type: String
-    },
-    colsSm: {
-      type: String
-    },
-    colsMd: {
-      type: String
-    },
-    colsLg: {
-      type: String
-    },
-    colsXl: {
-      type: String
-    },
-    rowClass: {
-      String
-    }
-  },
-  computed: {
-    customTag: function() {
-      return this.tag;
-    },
-    customNoGutters: function() {
-      return this.noGutters ? "no-gutters" : "";
-    },
-    customColsXs: function() {
-      return this.colsXs ? `row-cols-${this.colsXs}` : "";
-    },
-    customColsSm: function() {
-      return this.colsSm ? `row-cols-sm-${this.colsSm}` : "";
-    },
-    customColsMd: function() {
-      return this.colsMd ? `row-cols-md-${this.colsMd}` : "";
-    },
-    customColsLg: function() {
-      return this.colsLg ? `row-cols-lg-${this.colsLg}` : "";
-    },
-    customColsXl: function() {
-      return this.colsXl ? `row-cols-lx-${this.colsXl}` : "";
-    },
-    customRowClass: function() {
-      return this.rowClass;
-    }
-  },
-  render(h) {
+  functional: true,
+  props,
+  render(h, { props, data, children }) {
     return h(
-      this.customTag,
-      {
+      props.tag,
+      mergeData(data, {
         staticClass: "row",
-        class: [
-          this.customColsXs,
-          this.customColsSm,
-          this.customColsMd,
-          this.customColsLg,
-          this.customColsXl,
-          this.customNoGutters,
-          this.customRowClass
-        ]
-      },
-      this.$slots.default
+        class: customClass(props)
+      }),
+      children
     );
   }
 });
